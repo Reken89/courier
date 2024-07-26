d6c3c087be4eff621c82c201afb7cd4c0928d48c
user <user@MSI>
2024-07-26 19:56:10 +0300

    Правка шаблонов представления

--- views/add.php
+++ views/add.php
@@ -1,4 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+<script src="https://code.jquery.com/jquery-3.6.3.js"></script> 
 
 <div id="live_data"></div>
 
